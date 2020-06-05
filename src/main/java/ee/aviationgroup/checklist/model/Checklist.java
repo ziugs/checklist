bae1657bd08ed62b82dd29b45635ff86acdc1eb8
Mario <ziugs@hotmail.com>
2020-06-05 10:02:33 +0300

    update

--- src/main/java/ee/aviationgroup/checklist/model/Checklist.java
+++ src/main/java/ee/aviationgroup/checklist/model/Checklist.java
@@ -1,50 +1,23 @@
 package ee.aviationgroup.checklist.model;
 
 
+
+import java.util.List;
+
 public class Checklist {
-    public int checklist_id;
     private int id_log_day;
-    private String log_day;
-    private String element_name;
-    private int quantity;
-    private String element_group;
-    private boolean day_check;
-    private boolean night_check;
-    private String comment;
     private String crew_member;
-    private String pressure;
+    private String log_day;
+    private List<ChecklistElements> checklistElementsList;
 
-
-    public Checklist(){
-
+    public Checklist() {
     }
 
-    public Checklist(int quantity, boolean day_check, boolean night_check, String comment) {
-        this.quantity = quantity;
-        this.day_check = day_check;
-        this.night_check = night_check;
-        this.comment = comment;
-    }
-
-    public Checklist(int id_log_day, String log_day, String element_name, int quantity, String element_group, boolean day_check, boolean night_check, String comment, String crew_member, String pressure) {
+    public Checklist(int id_log_day, String crew_member, String log_day, List<ChecklistElements> checklistElementsList) {
         this.id_log_day = id_log_day;
-        this.log_day = log_day;
-        this.element_name = element_name;
-        this.quantity = quantity;
-        this.element_group = element_group;
-        this.day_check = day_check;
-        this.night_check = night_check;
-        this.comment = comment;
         this.crew_member = crew_member;
-        this.pressure = pressure;
-    }
-
-    public int getChecklist_id() {
-        return checklist_id;
-    }
-
-    public void setChecklist_id(int checklist_id) {
-        this.checklist_id = checklist_id;
+        this.log_day = log_day;
+        this.checklistElementsList = checklistElementsList;
     }
 
     public int getId_log_day() {
@@ -55,62 +28,6 @@ public class Checklist {
         this.id_log_day = id_log_day;
     }
 
-    public String getLog_day() {
-        return log_day;
-    }
-
-    public void setLog_day(String log_day) {
-        this.log_day = log_day;
-    }
-
-    public String getElement_name() {
-        return element_name;
-    }
-
-    public void setElement_name(String element_name) {
-        this.element_name = element_name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getElement_group() {
-        return element_group;
-    }
-
-    public void setElement_group(String element_group) {
-        this.element_group = element_group;
-    }
-
-    public boolean isDay_check() {
-        return day_check;
-    }
-
-    public void setDay_check(boolean day_check) {
-        this.day_check = day_check;
-    }
-
-    public boolean isNight_check() {
-        return night_check;
-    }
-
-    public void setNight_check(boolean night_check) {
-        this.night_check = night_check;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
     public String getCrew_member() {
         return crew_member;
     }
@@ -119,11 +36,19 @@ public class Checklist {
         this.crew_member = crew_member;
     }
 
-    public String getPressure() {
-        return pressure;
+    public String getLog_day() {
+        return log_day;
     }
 
-    public void setPressure(String pressure) {
-        this.pressure = pressure;
+    public void setLog_day(String log_day) {
+        this.log_day = log_day;
+    }
+
+    public List<ChecklistElements> getChecklistElementsList() {
+        return checklistElementsList;
+    }
+
+    public void setChecklistElementsList(List<ChecklistElements> checklistElementsList) {
+        this.checklistElementsList = checklistElementsList;
     }
 }
---
+++ src/main/java/ee/aviationgroup/checklist/model/ChecklistElements.java
@@ -0,0 +1,103 @@
+package ee.aviationgroup.checklist.model;
+
+
+public class ChecklistElements {
+    public int checklist_id;
+    private int id_log_day;
+    private String element_name;
+    private int quantity;
+    private String element_group;
+    private boolean day_check;
+    private boolean night_check;
+    private String comment;
+
+    private String pressure;
+
+    public ChecklistElements(){
+
+    }
+
+    public ChecklistElements(int checklist_id, int id_log_day, String element_name, int quantity, String element_group, boolean day_check, boolean night_check, String comment, String pressure) {
+        this.checklist_id = checklist_id;
+        this.id_log_day = id_log_day;
+        this.element_name = element_name;
+        this.quantity = quantity;
+        this.element_group = element_group;
+        this.day_check = day_check;
+        this.night_check = night_check;
+        this.comment = comment;
+        this.pressure = pressure;
+    }
+
+    public int getChecklist_id() {
+        return checklist_id;
+    }
+
+    public void setChecklist_id(int checklist_id) {
+        this.checklist_id = checklist_id;
+    }
+
+    public int getId_log_day() {
+        return id_log_day;
+    }
+
+    public void setId_log_day(int id_log_day) {
+        this.id_log_day = id_log_day;
+    }
+
+    public String getElement_name() {
+        return element_name;
+    }
+
+    public void setElement_name(String element_name) {
+        this.element_name = element_name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getElement_group() {
+        return element_group;
+    }
+
+    public void setElement_group(String element_group) {
+        this.element_group = element_group;
+    }
+
+    public boolean isDay_check() {
+        return day_check;
+    }
+
+    public void setDay_check(boolean day_check) {
+        this.day_check = day_check;
+    }
+
+    public boolean isNight_check() {
+        return night_check;
+    }
+
+    public void setNight_check(boolean night_check) {
+        this.night_check = night_check;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(String pressure) {
+        this.pressure = pressure;
+    }
+}
